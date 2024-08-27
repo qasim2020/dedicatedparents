a412d8f93309f0e050d0778a06471afcf4507448
qasim2020 <qasimali24@gmail.com>
2024-08-27 14:44:53 +0200

    made changes to view directory so all links are tailed to dpv2.js

--- dp2.js
+++ dp2.js
@@ -42,6 +42,21 @@ hbs.registerPartials(join(__dirname, 'views/partials'));
 // Serve static files
 app.use(express.static(join(__dirname, 'static')));
 
+hbs.registerHelper('getFormattedDateTimeMongoId', (objectId) => {
+    if ( objectId == null ) return null;
+    let date = new Date(parseInt(objectId.toString().substring(0, 8), 16) * 1000);
+    let dtg = date.toString().split(" ");
+    let obj = {
+        time: dtg[4],
+        date: dtg[2],
+        month: dtg[1],
+        yr: dtg[3]
+    }
+    let time = obj.time.split(":");
+    obj.time = time[0]+time[1]+ " hrs";
+    return `${obj.time} · ${obj.date} ${obj.month} ${obj.yr.slice(2,4)}`;
+});
+
 hbs.registerHelper('matchValues', (val1,val2) => {
     try {
         return val1.toString().toLowerCase()  == val2.toString().toLowerCase();
@@ -52,15 +67,82 @@ hbs.registerHelper('matchValues', (val1,val2) => {
 
 // Route handling
 app.get('/', async (req, res) => {
-    try {
-        req.params.brand = "dedicated_parents";
-        const data = await landingPage(req, res);
-        res.render('en', data);
-    } catch (e) {
-        console.error(e);
-        res.status(e.status || 400).send(e);
-    }
+    req.params.brand = "dedicated_parents";
+    const data = await landingPage(req, res);
+    res.render('home', data);
 });
+
+app.get('/events', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await events(req,res);
+    res.render('events', data);
+});
+
+app.get('/event', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await event(req,res);
+    res.render('event', data);
+});
+
+app.get('/team', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await team(req,res);
+    res.render('team', data);
+});
+
+app.get('/team-member/:slug', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await teamMember(req,res);
+    res.render('team-member', data);
+});
+
+app.get('/cause/:slug', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await cause(req,res);
+    res.render('cause', data);
+});
+
+app.get('/causes', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await causes(req,res);
+    res.render('causes', data);
+});
+
+app.get('/contact-us', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await contactUs(req,res);
+    res.render('contact', data);
+});
+
+app.get('/blog-post', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await blogPost(req,res);
+    res.render('blog', data);
+});
+
+app.get('/blogs', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await blogs(req,res);
+    res.render('blogs', data);
+});
+
+app.get('/all-pages', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await pages(req,res);
+    res.render('pages', data);
+});
+app.get('/page/:slug', async (req,res) => {
+    req.params.brand = "dedicated_parents";
+    const data = await page(req,res);
+    res.render('page', data);
+});
+
+app.post('/sendMsgToEmail', async (req,res) => {
+    res.status(200).send("integrate Email message feature");
+})
+app.post('/postComment', async (req,res) => {
+    res.status(200).send("integrate this feature");
+})
 
 // Error handling
 app.use((req, res) => {
